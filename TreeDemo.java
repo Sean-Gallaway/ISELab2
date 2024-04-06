65635fb09d4f3465952640f501a3979e1ead0c0b
Potat-OS1 <apotat.os1@gmail.com>
2024-04-06 16:04:37 -0700

    find method implemented and fix with insert

--- TreeDemo.java
+++ TreeDemo.java
@@ -20,15 +20,16 @@ class BinarySearchTree{
    */
    public Node insert(Node root, int value){
       //base case
-      if(root == null){
+      if(root == null) {
          root = new Node(value);
          return root;
       }
       
       //recursive step
-      if(value < root.value){
+      if(value < root.value) {
          root.left = insert(root.left, value); 
-      }else{
+      }
+      else {
          root.right = insert(root.right, value);
       }
       
@@ -70,27 +71,40 @@ class BinarySearchTree{
    */
    public boolean find(Node root, int key){
 	  //implement me
-      return false;           
+      if (root == null) {
+         return false;
+      }
+      if (root.value == key) {
+         return true;
+      }
+      if (key < root.value) {
+         return find(root.left, key);
+      }
+      else {
+         return find(root.right, key);
+      }
    }
    
    
    
    /*
    a method to find the node in the tree
-   with a smallest key
+   with the smallest key
    */
    public int getMin(Node root){
       //implement me
+      return 0;
    }
   
   
   
    /*
    a method to find the node in the tree
-   with a largest key
+   with the largest key
    */
    public int getMax(Node root){
 	  //implement me
+      return 0;
    }
    
    
@@ -100,25 +114,30 @@ class BinarySearchTree{
    is implemented
    */
    public Node delete(Node root, int key){
-      
-      if(root == null){
+      if(root == null) {
          return root;
-      }else if(key < root.value){
+      }
+      else if(key < root.value) {
          root.left = delete(root.left, key);
-      }else if(key > root.value){
+      }
+      else if(key > root.value) {
          root.right = delete(root.right, key);
-      }else{
+      }
+      else {
          //node has been found
-         if(root.left==null && root.right==null){
+         if(root.left==null && root.right==null) {
             //case #1: leaf node
             root = null;
-         }else if(root.right == null){
+         }
+         else if(root.right == null) {
             //case #2 : only left child
             root = root.left;
-         }else if(root.left == null){
+         }
+         else if(root.left == null) {
             //case #2 : only right child
             root = root.right;
-         }else{
+         }
+         else {
             //case #3 : 2 children
             root.value = getMax(root.left);
             root.left = delete(root.left, root.value);
@@ -136,12 +155,13 @@ class BinarySearchTree{
 public class TreeDemo{
    public static void main(String[] args){
       BinarySearchTree t1  = new BinarySearchTree();
-      t1.insert(24);
-      t1.insert(80);
-      t1.insert(18);
-      t1.insert(9);
-      t1.insert(90);
-      t1.insert(22);
+      t1.root = t1.insert(t1.root, 24);
+      t1.insert(t1.root, 80);
+      t1.insert(t1.root, 18);
+      t1.insert(t1.root, 9);
+      t1.insert(t1.root, 90);
+      t1.insert(t1.root, 22);
+      System.out.println(t1.find(t1.root, 90));
             
       System.out.print("in-order :   ");
       t1.inOrderTraversal(t1.root);
